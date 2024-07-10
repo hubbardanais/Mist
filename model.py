f9242a0a1e80efbdf07b82e2261433456d9cd1e3
hubba <hubba@hubba.com>
2024-07-09 21:05:08 -0700

    Added .gitignore

--- model.py
+++ model.py
@@ -1,25 +1,52 @@
-# """Models for movie ratings app."""
+"""Models for finding games owned in common"""
 
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
 
-# class User(db.Model):
-#     """A user."""
 
-#     __tablename__ = "users"
+class User(db.Model):
+    """A user."""
 
-#     user_id = db.Column(db.Integer,
-#                         autoincrement= True,
-#                         primary_key= True)
-#     email = db.Column(db.String, unique= True)
-#     password = db.Column(db.String)
+    __tablename__ = "users"
 
-#     ratings = db.relationship("Rating", back_populates="user")
+    id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, nullable=False,unique=True)
+    password = db.Column(db.String, nullable=False)
 
-#     def __repr__(self):
-#         return f'<User user_id={self.user_id} email={self.email}>'
+    steamid = db.Column(db.String, nullable=False, unique=True)
+    personaname = db.Column(db.String, nullable=False)
+    avatar = db.Column(db.String)
+    avatarmedium = db.Column(db.String)
+    profileurl = db.Column(db.String)
+
+    friends = db.relationship('Friend', back_populates="users")
+
+    def __repr__(self):
+        return f'<User id={self.id} personaname={self.personaname} steamid={self.steamid}>'
+    
+    
+class Friend(db.Model):
+    """a friend connection between to users"""
+
+    __tablename__ = "friends"
+
+    id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    user1 = db.Column(db.Integer, db.ForeignKey, nullable=False)
+    user2 = db.Column(db.Integer, db.ForeignKey, nullable=False)
+
+    # users = db.relationship('User', back_populates="friends")
+
+    def __repr__(self):
+        return f'<Friend id={self.id} user1={self.user1} user2={self.user2}>'
+    
+
+
     
 
 # class Movie(db.Model):
@@ -59,23 +86,23 @@
 #         return f'<Rating rating_id={self.rating_id} score={self.score}>'
 
 
-# def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
-#     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-#     flask_app.config["SQLALCHEMY_ECHO"] = echo
-#     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-#     db.app = flask_app
-#     db.init_app(flask_app)
+    db.app = flask_app
+    db.init_app(flask_app)
 
-#     print("Connected to the db!")
-
-
-# if __name__ == "__main__":
-#     from server import app
+    print("Connected to the db!")
 
 
-#     # Call connect_to_db(app, echo=False) if your program output gets
-#     # too annoying; this will tell SQLAlchemy not to print out every
-#     # query it executes.
+if __name__ == "__main__":
+    from server import app
 
-#     connect_to_db(app)
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
